08bbaebbdf98747dc8095deaf4a18b852b33f681
mariuspdv <mariuspdv@yahoo.com>
2020-11-02 17:08:03 +0100

    class Firm done! basic hiring/firing mechanics

---
+++ firm.py
@@ -0,0 +1,49 @@
+# Definition of firms
+
+class Firm:
+
+    def __init__(self, workers, wages, productivity, profits=0):
+        self.workers = workers
+        self.wages = wages
+        self.productivity = productivity
+        self.profits = profits
+        self.history = []
+        self.add_to_history()
+
+    def __str__(self):
+        return f'Employés: {self.workers}'
+
+    def add_to_history(self):
+        self.history.append({'workers': self.workers,
+                             'wages': self.wages,
+                             'productivity': self.productivity,
+                             'profits': self.profits})
+
+    def get_from_history(self, thing, index, default=None):
+        if len(self.history) == 0:
+            return default
+        return self.history[index][thing]
+
+    def set_supply(self):
+        '''offre totale'''
+        return self.workers * self.productivity
+
+    def update_firm(self, sold, prices, pops):
+        '''changer l'état de la firme'''
+        costs = self.wages * self.workers
+        revenues = sold * prices
+        self.profits = revenues - costs
+        prev_profit = self.get_from_history('profits', -1, 0)
+        if self.profits > prev_profit and pops['population'] > self.workers:
+            self.workers += 1
+        elif self.profits < prev_profit and self.workers > 1:
+            self.workers -= 1
+
+
+if __name__ == '__main__':
+    f = Firm(workers=15, wages=1, productivity=1)
+    print(f.set_supply())
+    f.add_to_history()
+    f.add_to_history()
+    print(f.history)
+    print(f)
--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # This is the main loop
+from firm import Firm
+
 PRICE_INC = 0.0001
 EPS = 0.001
 
-firm = {'workers': 10, 'productivity': 1, 'profits': [], 'wages': 1}
+firm_1 = Firm(workers=15, wages=1, productivity=1)
 pop = {'needs': 0.8, 'population': 20, 'income': 1}
 prev_prices = 0.95
-
-
-def set_supply(firms):
-    '''offre totale à partir des firms'''
-    return firms['workers'] * firms['productivity']
 
 
 def value_goods(goods, prices):
@@ -27,22 +24,8 @@ def set_demand(pops, prices):
     return demand * pops['population']
 
 
-def update_firm(firms, sold, prices, pops):
-    '''changer l'état de la firme'''
-    costs = firms['wages'] * firms['workers']
-    revenues = sold * prices
-    profit = revenues - costs
-    prev_profit = firms['profits'][-1] if len(firms['profits']) > 0 else 0
-    firms['profits'].append(profit)
-    if profit > prev_profit and pops['population'] > firms['workers']:
-        firms['workers'] += 1
-    elif profit < prev_profit and firms['workers'] > 1:
-        firms['workers'] -= 1
-
-
-def tick(t, firm, prev_prices, pop):
-    supply = set_supply(firm)
-    prices = prev_prices
+def tick(t, firm, prices, pop):
+    supply = firm.set_supply()
     tot_demand = set_demand(pop, prices)
 
     while abs(tot_demand - supply) >= EPS:
@@ -55,12 +38,13 @@ def tick(t, firm, prev_prices, pop):
 
     print(f"prix: {prices}, quantité: {tot_demand}")
 
-    update_firm(firm, tot_demand, prices, pop)
+    firm.update_firm(tot_demand, prices, pop)
+    firm.add_to_history()
     print(firm)
     return prices
 
 
-prev_prices = tick(0, firm, prev_prices, pop)
-prev_prices = tick(0, firm, prev_prices, pop)
-prev_prices = tick(0, firm, prev_prices, pop)
-prev_prices = tick(0, firm, prev_prices, pop)
+prev_prices = tick(0, firm_1, prev_prices, pop)
+prev_prices = tick(0, firm_1, prev_prices, pop)
+prev_prices = tick(0, firm_1, prev_prices, pop)
+prev_prices = tick(0, firm_1, prev_prices, pop)
