214fb76b7daebd4fb91ca53044b1a8b96162d542
mariuspdv <mariuspdv@yahoo.com>
2020-11-02 16:09:38 +0100

    market_and_firm_adjustment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 PRICE_INC = 0.0001
 EPS = 0.001
 
-firm = {'workers': 15, 'productivity': 1, 'profits': [], 'wages': 1.2}
+firm = {'workers': 10, 'productivity': 1, 'profits': [], 'wages': 1}
 pop = {'needs': 0.8, 'population': 20, 'income': 1}
 prev_prices = 0.95
 
@@ -27,12 +27,17 @@ def set_demand(pops, prices):
     return demand * pops['population']
 
 
-def update_firm(firms, sold, prices):
+def update_firm(firms, sold, prices, pops):
     '''changer l'état de la firme'''
     costs = firms['wages'] * firms['workers']
     revenues = sold * prices
     profit = revenues - costs
+    prev_profit = firms['profits'][-1] if len(firms['profits']) > 0 else 0
     firms['profits'].append(profit)
+    if profit > prev_profit and pops['population'] > firms['workers']:
+        firms['workers'] += 1
+    elif profit < prev_profit and firms['workers'] > 1:
+        firms['workers'] -= 1
 
 
 def tick(t, firm, prev_prices, pop):
@@ -45,16 +50,17 @@ def tick(t, firm, prev_prices, pop):
             prices += PRICE_INC
         else:
             prices -= PRICE_INC
-        print(prices)
+#        print(prices)
         tot_demand = set_demand(pop, prices)
 
     print(f"prix: {prices}, quantité: {tot_demand}")
 
-    update_firm(firm, tot_demand, prices)
+    update_firm(firm, tot_demand, prices, pop)
     print(firm)
     return prices
 
 
+prev_prices = tick(0, firm, prev_prices, pop)
 prev_prices = tick(0, firm, prev_prices, pop)
 prev_prices = tick(0, firm, prev_prices, pop)
 prev_prices = tick(0, firm, prev_prices, pop)
