4c76dc85e9a54ddcd4ead8247f632bcf77a8d35a
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-11-05 09:56:18 +0100

    Commencé à créer les "goods" et "needs" multi-produit

--- pop.py
+++ pop.py
@@ -8,12 +8,21 @@ class Pop(Historizor):
     def __init__(self, needs, population, income):
         super().__init__()
         self.needs = needs
+        self._levels = set(l for _, l, _ in needs)
+        self._needs_per_level = self.breakdown_needs(self._levels, self.needs)
         self.population = population
         self.income = income
         self.add_to_history()
 
     def __str__(self):
         return f'PoP: Hello, we are {self.population}'
+
+    @staticmethod
+    def breakdown_needs(levels, needs):
+        needs_per_level = {l: [] for l in levels}
+        for good, level, qty in needs:
+            needs_per_level[level].append((good, qty))
+        return needs_per_level
 
     def value_goods(self, goods, prices):
         return goods * prices
