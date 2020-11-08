e99996770a5bacf02ad15481e2ac154277c034ae
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-11-08 11:00:02 +0100

    Clean code and added comments

--- world.py
+++ world.py
@@ -1,20 +1,26 @@
 # It says hello
 from markets import Markets
 
+
 class World:
     PRICE_INC = 0.0001
     EPS = 0.001
+    PRICE_CHANGE_CEILING = 1.2
+    PRICE_CHANGE_FLOOR = 0.8
 
     def __init__(self, goods, firms, pops, prices):
+        # Core properties
         self.goods = goods
         self.firms = firms
         self.pops = pops
         self.prices = prices
 
+        # Computed aggregates
         self.tot_demand = {}
         self.tot_supply = {}
         self.tot_population = 0
 
+        # Technical logistics
         self.history = []
 
     def add_to_history(self):
@@ -28,6 +34,8 @@ class World:
         self.tot_population = sum(pop.population for pop in self.pops)
 
     def clear_goods_market(self):
+        """Adjust aggregated demand, supply and prices on good markets"""
+
         def aggregate_supply():
             supply = Markets(self.goods)
             for firm in self.firms:
@@ -40,39 +48,60 @@ class World:
                 demand += pop.set_demand(prices)
             return demand
 
-        max_prices = {good: price * 1.2 for good, price in self.prices.items()}
-        min_prices = {good: price * 0.8 for good, price in self.prices.items()}
+        # Limit price changes in one tick to a band between PRICE_CHANGE_FLOOR and CEILING
+        max_prices = {good: price * self.PRICE_CHANGE_CEILING for good, price in self.prices.items()}
+        min_prices = {good: price * self.PRICE_CHANGE_FLOOR for good, price in self.prices.items()}
 
+        # Compute the aggregated supply of goods over all the firms
         tot_supply = aggregate_supply()
-        tot_demand = aggregate_demand(self.prices)
 
-        # sum(abs(tot_demand[good] - tot_supply[good]) for good in goods) >= EPS
+        #
+        # Adjust iteratively goods' prices to equilibrate goods demand and supply
+        # within acceptable price movement
+
+        # Compute the aggregated demand over all the pops, given a set of prices
+        tot_demand = aggregate_demand(self.prices)
         loop = True
         while loop:
+            # The loop will end when all goods market are at equilibrium (supply == demand)
+            # or if the prices have stopped changing from iteration to another (which happens
+            # when they have reached their floor or ceiling for the tick
             loop = False
+            # Each market good is considered. We stop when all price adjustments have reached their finish condition
             for good in self.goods:
+                # Because prices chang by a discrete increment, mathematical equality spply==demand can not be reached
+                # We tolerate a difference of a small number EPS
                 if abs(tot_demand[good] - tot_supply[good]) <= self.EPS:
+                    # supply == demand : no need to change price change
                     continue
                 if tot_demand[good] > tot_supply[good] and self.prices[good] < max_prices[good]:
+                    # Adjust price if we are still in the acceptable rang
                     self.prices[good] += self.PRICE_INC
                     loop = True
                 elif tot_demand[good] < tot_supply[good] and self.prices[good] > min_prices[good]:
+                    # Adjust price if we are still in the acceptable rang
                     self.prices[good] -= self.PRICE_INC
                     loop = True
-            #        print(prices)
+            # Compute the aggregated demand over all the pops, given the new set of prices
             tot_demand = aggregate_demand(self.prices)
 
         self.tot_demand = tot_demand
         self.tot_supply = tot_supply
 
     def clear_labor_market(self):
+        """Adjust aggregated demand, supply and prices on labor market"""
         for firm in self.firms:
             firm.update_firm(self.tot_demand, self.prices, self)
 
-    def tick(self, t):
+    def tick(self, t: int):
+        # Compute useful aggregate(s)
         self.compute_tot_population()
+
+        # Core mechanism
         self.clear_goods_market()
         self.clear_labor_market()
+
+        # Technical logistics
         self.add_to_history()
 
     def summary(self):
