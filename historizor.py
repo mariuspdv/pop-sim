be3e67a7df628e7369040d5dfba02d71a057c853
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2020-11-30 18:01:29 +0100

    Enlevé le bug d'historisation des Dict et List

--- historizor.py
+++ historizor.py
@@ -6,7 +6,8 @@ class Historizor:
         self.history = []
 
     def add_to_history(self):
-        self.history.append({k: v for k, v in vars(self).items() if not (k == 'history' or k.startswith('_'))})
+        self.history.append({k: v.copy() if type(v) is dict or type(v) is list else v
+                             for k, v in vars(self).items() if not (k == 'history' or k.startswith('_'))})
 
     def get_from_history(self, thing, index, default=None):
         if len(self.history) == 0:
